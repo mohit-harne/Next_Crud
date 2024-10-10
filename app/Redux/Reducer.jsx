bfcc6fe87d1e1a3f72bbce655193228556030d70
mohit-harne <mohitharne@gmail.com>
2024-10-10 17:59:43 +0530

    new - changes

--- app/Redux/Reducer.jsx
+++ app/Redux/Reducer.jsx
@@ -3,7 +3,8 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 // Get API base URL from environment variables (use in production)
-const apiBaseUrl =  process.env.NEXT_PUBLIC_API_BASE_URL   || 'http://localhost:3000' ;
+const apiBaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:3000';
+
 
 // Thunks for asynchronous actions
 
