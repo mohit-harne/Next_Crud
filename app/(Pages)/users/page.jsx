f2af3e5bc1445bda03e8e0386e7e3a8c54ec00d9
mohit-harne <mohitharne@gmail.com>
2024-11-12 11:57:13 +0530

    swr testing for cache fix

--- app/(Pages)/users/page.jsx
+++ app/(Pages)/users/page.jsx
@@ -1,18 +1,17 @@
-'use client';
-import { useEffect, useState, useCallback } from 'react';
+'use client'
+import { useState, useEffect, useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchUserList, selectUserList, deleteUser, selectLoadingStatus, selectErrorMessage } from '../../Redux/Reducer';
-import '../../globals.css';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Link from 'next/link';
 import { Modal } from 'flowbite-react';
 import React from "react";
-import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, User, Chip, Tooltip } from "@nextui-org/react";
+import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, Chip, Tooltip } from "@nextui-org/react";
 import { EditIcon } from "./EditIcon";
 import { DeleteIcon } from "./DeleteIcon";
 import { EyeIcon } from "./EyeIcon";
 import { columns } from "./data";
+import useSWR, { mutate } from 'swr';
 
 const statusColorMap = {
     Active: "success",
@@ -21,39 +20,31 @@ const statusColorMap = {
 };
 
 const UsersPage = () => {
-    const dispatch = useDispatch();
-    const users = useSelector(selectUserList);
-    const loading = useSelector(selectLoadingStatus);
-    const error = useSelector(selectErrorMessage);
-
+    const { data: users, error: fetchError, isLoading } = useSWR('/api/users', fetcher);
     const [openModal, setOpenModal] = useState(false);
     const [selectedUserId, setSelectedUserId] = useState(null);
     const [selectedUserName, setSelectedUserName] = useState('');
 
-    useEffect(() => {
-        dispatch(fetchUserList());
-    }, [dispatch]);
-
-    const handleDelete = () => {
+    const handleDelete = async () => {
         if (!selectedUserId) {
             console.error("User ID is missing!");
             toast.error("User ID is missing!");
             return;
         }
-    
-        dispatch(deleteUser(selectedUserId))
-            .then(() => {
-                toast.success("User deleted successfully!");
-                dispatch(fetchUserList());  // Refresh the user list after delete
-                setOpenModal(false);        // Close the modal
-            })
-            .catch((error) => {
-                console.error("Delete error:", error);
-                toast.error("Failed to delete user. Please try again.");
-            });
+
+        try {
+            // Perform delete request
+            await fetch(`/api/users/${selectedUserId}`, { method: 'DELETE' });
+            toast.success("User deleted successfully!");
+
+            // Trigger a re-fetch of the users after deletion
+            mutate('/api/users');
+            setOpenModal(false); // Close the modal
+        } catch (error) {
+            console.error("Delete error:", error);
+            toast.error("Failed to delete user. Please try again.");
+        }
     };
-    
-    
 
     const openDeleteModal = (userId, userName) => {
         setSelectedUserId(userId);
@@ -70,10 +61,8 @@ const UsersPage = () => {
                     <div>
                         <h1 className='text-xl font-bold tracking-wide'>{user.first_name}</h1>
                         <h3 className='text-sm'>{user.email}</h3>
-                    </div>                  
-</div>
-
-                );
+                    </div>
+                </div>);
             case "role":
                 return (
                     <div className="flex flex-col">
@@ -115,71 +104,70 @@ const UsersPage = () => {
     }, []);
 
     return (
-      <div className="py-4 pt-5 main">
-        <div className="shadow-md rounded-lg border border-gray-300">
-            <div className="px-4 py-3 bg-gray-800 rounded-t-lg">
-                <Link href="#" className="bg-green-500 hover:bg-green-600 px-4 py-2 rounded shadow-md">
-                    Add New User [+]
-                </Link>
+        <div className="py-4 pt-5 main">
+            <div className="shadow-md rounded-lg border border-gray-300">
+                <div className="px-4 py-3 bg-gray-800 rounded-t-lg">
+                    <Link href="#" className="bg-green-500 hover:bg-green-600 px-4 py-2 rounded shadow-md">
+                        Add New User [+]
+                    </Link>
+                </div>
+                <div className="p-4" style={{ minHeight: '400px' }}>
+                    {isLoading ? (
+                        <div className="text-center py-4">Loading...</div>
+                    ) : fetchError ? (
+                        <div className="text-center py-4 text-red-500">Error: {fetchError}</div>
+                    ) : (
+                        <Table aria-label="User Table" className="overflow-x-auto">
+                            <TableHeader>
+                                {columns.map((column) => (
+                                    <TableColumn key={column.uid} align={column.uid === "actions" ? "center" : "start"} className='text-left tracking-widest text-xl'>
+                                        {column.name}
+                                    </TableColumn>
+                                ))}
+                            </TableHeader>
+                            <TableBody style={{ maxHeight: '300px', overflowY: 'auto' }}>
+                                {users?.map((user) => (
+                                    <TableRow key={user._id}>
+                                        {columns.map((column) => (
+                                            <TableCell key={column.uid}>
+                                                {renderCell(user, column.uid)}
+                                            </TableCell>
+                                        ))}
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    )}
+                </div>
             </div>
-            <div className="p-4" style={{ minHeight: '400px' }}>
-                {loading ? (
-                    <div className="text-center py-4">Loading...</div>
-                ) : error ? (
-                    <div className="text-center py-4 text-red-500">Error: {error}</div>
-                ) : (
-                    <Table aria-label="User Table" className="overflow-x-auto">
-                        <TableHeader>
-                            {columns.map((column) => (
-                                <TableColumn key={column.uid} align={column.uid === "actions" ? "center" : "start"} className='text-left tracking-widest text-xl'>
-                                    {column.name}
-                                </TableColumn>
-                            ))}
-                        </TableHeader>
-                        <TableBody style={{ maxHeight: '300px', overflowY: 'auto' }}>
-                            {users.map((user) => (
-                                <TableRow key={user._id}>
-                                    {columns.map((column) => (
-                                        <TableCell key={column.uid}>
-                                            {renderCell(user, column.uid)}
-                                        </TableCell>
-                                    ))}
-                                </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                )}
-            </div>
+            <ToastContainer className="z-50 mt-[100px]" />
+
+            <Modal
+                className="w-1/2 mx-auto my-auto h-fit bg-transparent"
+                show={openModal}
+                position="center"
+                onClose={() => setOpenModal(false)}
+            >
+                <Modal.Header className="text-black text-center">Confirm Delete</Modal.Header>
+                <Modal.Body className="text-black text-center">
+                    Are you sure you want to delete profile of <span className='font-bold'>{selectedUserName}</span>?
+                </Modal.Body>
+                <Modal.Footer className="flex justify-center gap-8">
+                    <button onClick={handleDelete} className="bg-red-500 py-2 px-4 rounded-2xl shadow-lg text-white hover:scale-105 transition-all duration-300">Delete</button>
+                    <button
+                        color="gray"
+                        onClick={() => setOpenModal(false)}
+                        className="text-black py-2 px-4 rounded-2xl border-2 border-green-500 hover:scale-105 transition-all duration-300 shadow-lg"
+                    >
+                        Cancel
+                    </button>
+                </Modal.Footer>
+            </Modal>
         </div>
-        <ToastContainer className="z-50 mt-[100px]" />
-      
-        <Modal
-    className="w-1/2 mx-auto my-auto h-fit bg-transparent"
-    
-    show={openModal}
-    position="center"
-    onClose={() => setOpenModal(false)}
->
-    <Modal.Header className="text-black text-center">Confirm Delete</Modal.Header>
-    <Modal.Body className="text-black text-center">
-        Are you sure you want to delete profile of <span className='font-bold'>{selectedUserName}</span> ?
-    </Modal.Body>
-    <Modal.Footer className="flex justify-center gap-8">
-        <button onClick={handleDelete} className="bg-red-500 py-2 px-4 rounded-2xl shadow-lg text-white hover:scale-105 transition-all duration-300">Delete</button>
-        <button
-            color="gray"
-            onClick={() => setOpenModal(false)}
-            className="text-black py-2 px-4 rounded-2xl border-2 border-green-500 hover:scale-105 transition-all duration-300 shadow-lg"
-        >
-            Cancel
-        </button>
-    </Modal.Footer>
-</Modal>
-
-
-       </div>
-     
     );
 };
+
+// Helper fetcher function for SWR
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export default UsersPage;
